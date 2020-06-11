7865f66a1d83bb8a32838fa8f35ecdb2b075d18c
hardant <122138292@qq.com>
2020-06-11 13:45:55 +0800

    Dapper

---
+++ Sample05/Sample05/Common/Db.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Sample05
+{
+    public class Db
+    {
+        public static readonly string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+        public static SqlConnection OpenConnection()
+        {
+            var connection = new SqlConnection(connectionString);
+            connection.Open();
+            return connection;
+        }
+        public static SqlConnection ClosedConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
