e032c9ba4d9dc47c5acb145e8563e31fd49ef6c3
hardant <122138292@qq.com>
2020-06-11 13:13:45 +0800

    测试代码原始版本

---
+++ Sample01/Sample01/Controllers/ContentController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Sample01.Models;
+
+namespace Sample01.Controllers
+{
+    public class ContentController : Controller
+    {
+        private readonly List<Content> contents;
+
+        public ContentController(IOptions<List<Content>> option)
+        {
+            contents = option.Value;
+        }
+
+        /// <summary>
+        /// 首页显示
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            return View(new ContentViewModel { Contents = contents });
+
+            //var contents = new List<Content>();
+            //for (int i = 1; i < 11; i++)
+            //{
+            //    contents.Add(new Content { Id = i, title = $"{i}的标题", content = $"{i}的内容", status = 1, add_time = DateTime.Now.AddDays(-i) });
+            //}
+            //return View(new ContentViewModel { Contents = contents });
+        }
+    }
+}
